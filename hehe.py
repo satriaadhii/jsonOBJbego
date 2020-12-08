ab3e9bb82657b59cad52402a7684e3a8eadf8f47
satriaadhii <sakharisma@gmail.com>
2020-12-08 15:40:10 +0700

    complete da detail

--- hehe.py
+++ hehe.py
@@ -1,5 +1,6 @@
 import json 
 
+# when you found another converting problem, just add in here
 def fixer(e, data):
     if "Expecting ',' delimiter" in str(e):
         fix_data = data+"}"
@@ -13,9 +14,9 @@ def fixer(e, data):
         fix_data = data+'"' 
     else:
         fix_data = "unfix"
-
     return fix_data
 
+# function to validate the json string, and fix it over and over again until able to convert to jsonObj
 def validation(string):
     data = string
     while True:
@@ -26,15 +27,17 @@ def validation(string):
         except Exception as e:
             data = fixer(e, data)
 
-jsstr = {
+js = {
     "asep" : "asep",
     "foo" : "bar",
     "nani" : "hahay",
 }
 
-string = json.dumps(jsstr)
-missing_string = string[:-18]
-print('missing string:', missing_string)
+string = json.dumps(js) # convert json object to json string, its a should when in the future u need to send a json.
+missing_string = string[:-18] # this variable used to test the function by define how much char is missing. 
+
+print('missing string:', missing_string) # we print the rest string
+
 try:
     jsob = json.loads(missing_string)
     print(jsob)
